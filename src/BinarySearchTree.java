ef549e87a12bf42df92612ccd746922923725d5f
mertmahanoglu <mahanoglumert73@gmail.com>
2020-10-23 23:20:59 +0300

    yeni

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -9,7 +9,12 @@ public class BinarySearchTree {
      //----------------- KİŞİ BİLGİLERİ --------------
     public  Node root; //KÖK
     public  Node head; //BAŞ
+    public static Node oncekiNode = null;
     
+    int dugumSay = 1;
+    int dugumIndex = 0;
+    
+    Node currentNode;
     
     String BSTad;
     String BSTadres;
@@ -38,10 +43,15 @@ public class BinarySearchTree {
     String BSTbitisTarih;
     double BSTnotOrt;
 
-    public BinarySearchTree() {
-        root=null; //KÖK EN BAŞTA BOŞ NULL'A EŞİTLİYORUZ
+   /* public BinarySearchTree(String ad, String adres, String telefonNo, String email, String dogumTarihi, String yabanciDil, int yabanciDilSay, String ehliyet, String isYeriAd, String isYeriAdres, String isYeriTelefon, String pozisyon, int calismaSuresi, String okulAd, String bolum, String baslangicTarih, String bitisTarih, double notOrt) {
+       
+       
+        currentNode = new Node(null, null, ad, adres, telefonNo, email, dogumTarihi, yabanciDil, yabanciDilSay, ehliyet, isYeriAd, isYeriAdres, isYeriTelefon, pozisyon, calismaSuresi, okulAd, bolum, baslangicTarih, bitisTarih, notOrt);
+        currentNode.sag = null;
+        currentNode.sol = null;
+        
     }
-    
+    */
     
     
      void BstToLinked(Node root) 
@@ -51,26 +61,27 @@ public class BinarySearchTree {
             return; 
   
         // Recursively convert right subtree 
-        BstToLinked(root.sag); 
+        BstToLinked(root.sol); 
   
         // insert root into DLL 
-        root.sag = head; 
-  
-        // Change left pointer of previous head 
-        if (head != null) 
-            head.sol = root; 
-  
-        // Change head of Doubly linked list 
-        head = root; 
-  
-        // Recursively convert left subtree 
-        BstToLinked(root.sol); 
+        
+        if (oncekiNode==null) {
+            head=root;
+        }
+        else
+        {
+        root.sol = oncekiNode;
+        oncekiNode.sag = root;
+        }
+        oncekiNode = root;
+        
+        BstToLinked(root.sag);
     } 
     
 
     //ADDED BY BORAN 
     
-    void DugumuSil 
+  /*  void DugumuSil 
     (int konum ) { 
 
 // sistemdeki bilgilerini kaldırma 
@@ -106,11 +117,12 @@ gecicidugum.sag = sag; // Bagi listeden kes
     }
     
     //END POINT
-
-      void listeyiYazdir(Node head) 
+*/
+      void listeyiYazdir(Node node) 
     { 
+        //reachNode(0);
         System.out.println("Extracted Double Linked List is : "); 
-        while (head != null) { 
+        while (node !=null) { 
            System.out.print(head.ad + " ");
              System.out.print(head.adres + " "); 
               System.out.print(head.baslangicTarih + " "); 
@@ -128,81 +140,213 @@ gecicidugum.sag = sag; // Bagi listeden kes
                           System.out.print(head.pozisyon + " "); 
                            System.out.print(head.telefonNo + " "); 
                            System.out.print(head.yabanciDil + " ");
-                           System.out.print(head.yabanciDilSay + " ");
+                           System.out.print(head.yabanciDilSay + " \n");
+                          node = node.sag;
                           
                 
-            head = head.sag; 
+          //  nextNode();
         } 
     } 
-  
-    public static void main(String[] args) {
-        
-          Scanner scanner = new Scanner(System.in);
-        BinarySearchTree bstTree = new BinarySearchTree();
-        
-     
-        
-              //----------------- KİŞİ BİLGİLERİ INPUT-------------- 
+
+    public int getDugumSay() {
+        return dugumSay;
+    }
+
+    public void setDugumSay(int dugumSay) {
+        this.dugumSay = dugumSay;
+    }
+      
+      
+      //WRITTEN BY MERT MAHANOĞLU
+      public void nextNode()
+      {
+          if (currentNode.sag==null) {
+              System.out.println("Dugum bulunamadi");
+          }
+          else
+          {
+          currentNode = currentNode.sag;
+          dugumIndex++;
+          }
+      }
+      
+      public void previousNode()
+      {
+      
+          if (currentNode.sol==null) {
+              System.out.println("Dugum bulunamadi");
+          }
+          else
+          {
+          currentNode = currentNode.sol;
+          dugumIndex--;
+          }
+      
+      }
+      
+      
+    /*  public void dugumEkle(String ad, String adres, String telefonNo, String email, String dogumTarihi, String yabanciDil, int yabanciDilSay, String ehliyet, String isYeriAd, String isYeriAdres, String isYeriTelefon, String pozisyon, int calismaSuresi, String okulAd, String bolum, String baslangicTarih, String bitisTarih, double notOrt)
+      {
+          if (currentNode.sag==null) {
+              currentNode=currentNode.sag=new Node(currentNode,null,ad, adres, telefonNo, email, dogumTarihi, yabanciDil, yabanciDilSay, ehliyet, isYeriAd, isYeriAdres, isYeriTelefon, pozisyon, calismaSuresi, okulAd, bolum, baslangicTarih, bitisTarih, notOrt);
+              
+          }
+          else
+          {
+          currentNode = currentNode.sag= new Node(currentNode, currentNode.sag, ad, adres, telefonNo, email, dogumTarihi, yabanciDil, yabanciDilSay, ehliyet, isYeriAd, isYeriAdres, isYeriTelefon, pozisyon, calismaSuresi, okulAd, bolum, baslangicTarih, bitisTarih, notOrt);
+          }      
+          dugumSay++;
+          dugumIndex++;
+          
+      }*/
+      
+      public void reachNode(int index)
+      {
+          while (dugumIndex!=index) {   
+              
+              if (dugumIndex<index) {
+                  
+                  nextNode();
+                  
+              }
+              else if (dugumIndex>index) {
+                  
+                  previousNode();
+                  
+              }
+              
+              
+          }
+      
+      }
+      
+      
+      public void bilgiInput(BinarySearchTree tree)
+      {
+       
+         
+          System.out.println("Bilgi girmek ister misiniz?");
+          int bilgi = scanner.nextInt();
+          
+          while (bilgi!=0) {              
+                 //----------------- KİŞİ BİLGİLERİ INPUT-------------- 
     System.out.println("Adınızı giriniz :");
-    String ad = bstTree.BSTad = scanner.nextLine();
+    String ad = scanner.next();
     
      System.out.println("Adresinizi giriniz :");
-    String adres =bstTree.BSTadres = scanner.nextLine();
+    String adres =scanner.next();
     
      System.out.println("Telefon numaranızı giriniz :");
-    String telNo = bstTree.BSTtelefonNo = scanner.nextLine(); 
+    String telNo = scanner.next();
     
      System.out.println("E-mailinizi giriniz :");
-    String mail = bstTree.BSTemail = scanner.nextLine();
+    String mail = scanner.next();
     
      System.out.println("Doğum tarihinizi giriniz :");
-   String dogTar =  bstTree.BSTdogumTarihi = scanner.nextLine();
+   String dogTar =  scanner.next();
     
      System.out.println("Bildiğiniz yabancı dilleri  giriniz :");
-    String dil = bstTree.BSTyabanciDil = scanner.nextLine();
+    String dil = scanner.next();
     
      System.out.println("Ehliyetiniz var mı? (VAR/YOK)");
-    String ehliyet = bstTree.BSTehliyet = scanner.nextLine();
+    String ehliyet = scanner.next();
     
-     //----------------- KİŞİ BİLGİLERİ INPUT BİTİŞ-------------- 
+ //----------------- KİŞİ BİLGİLERİ INPUT BİTİŞ-------------- 
      
-     
-         //-----------------ÖNCEKİ İŞ YERİ BİLGİLERİ INPUT ------------------
+ //-----------------ÖNCEKİ İŞ YERİ BİLGİLERİ INPUT ------------------
     
      System.out.println("İş yeri adınız giriniz :");
-    String isAd = bstTree.BSTisYeriAd = scanner.nextLine();
+    String isAd = scanner.next();
     
      System.out.println("İş yeri adresiniz giriniz :");
-    String adresIs = bstTree.BSTisYeriAdres = scanner.nextLine();
+    String adresIs = scanner.next();
     
      System.out.println("Pozisyon/göreviniz giriniz :");
-    String gorev = bstTree.BSTpozisyon = scanner.nextLine();
+    String gorev = scanner.next();
     
      System.out.println("Çalışma süreniz giriniz :");
-    int sure = bstTree.BSTcalismaSuresi = scanner.nextInt();
+    int sure = scanner.nextInt();
     
-        //-----------------ÖNCEKİ İŞ YERİ BİLGİLERİ BİTİŞ------------------
+//-----------------ÖNCEKİ İŞ YERİ BİLGİLERİ BİTİŞ------------------
     
         
-        //---------------------- EĞİTİM BİLGİLERİ INPUT-----------------------
+//---------------------- EĞİTİM BİLGİLERİ INPUT-----------------------
     
         
         System.out.println("Okul adınızı giriniz :");
-   String adOkul = bstTree.BSTokulAd = scanner.nextLine();
+   String adOkul =scanner.next();
     
      System.out.println("Bölümünüzü giriniz :");
-    String bolum = bstTree.BSTbolum = scanner.nextLine();
+    String bolum =scanner.next();
     
      System.out.println("Okula giriş tarihiniz giriniz :");
-   String basTar =  bstTree.BSTbaslangicTarih = scanner.nextLine();
+   String basTar =   scanner.next();
     
      System.out.println("Mezuniyet tarihiniz giriniz :");
-    String bitTar = bstTree.BSTbitisTarih = scanner.nextLine();
+    String bitTar =  scanner.next();
     
     System.out.println("Not ortalamanız giriniz :");
-     double notOrt = bstTree.BSTnotOrt = scanner.nextDouble();
+     double notOrt = scanner.nextDouble();
      
-     bstTree.root = new Node( ad, adres, telNo, mail, dogTar, dil, sure, ehliyet, isAd, adres, telNo, isAd, sure, adOkul, bolum, basTar, bitTar, notOrt);
+              if (head==null) {
+                  Node ilk = new Node(ad, adres, BSTtelefonNo, mail, BSTdogumTarihi, BSTyabanciDil, BSTyabanciDilSay, ehliyet, BSTisYeriAd, BSTisYeriAdres, BSTisYeriTelefon, BSTpozisyon, BSTcalismaSuresi, BSTokulAd, bolum, BSTbaslangicTarih, BSTbitisTarih, notOrt);
+                  head = ilk;
+              }
+              else
+              {
+                   Node aktif = head;
+                   Node eklenecek = new Node(ad, adres, BSTtelefonNo, mail, BSTdogumTarihi, BSTyabanciDil, BSTyabanciDilSay, ehliyet, BSTisYeriAd, BSTisYeriAdres, BSTisYeriTelefon, BSTpozisyon, BSTcalismaSuresi, BSTokulAd, bolum, BSTbaslangicTarih, BSTbitisTarih, notOrt);
+      
+                       while (aktif.sag!=null) {                  
+                              aktif = aktif.sag;
+                                          }
+                              aktif.sag = eklenecek;
+              }
+     
+ 
+       
+        
+        
+     //tree.root = new Node(ad, adres, telNo, mail, dogTar, dil, sure, ehliyet, isAd, adres, telNo, isAd, sure, adOkul, bolum, basTar, bitTar, notOrt);
+     
+    
+     
+     System.out.println("Liste1 : ");
+     tree.BstToLinked(tree.root);
+     tree.listeyiYazdir(head);
+     
+      System.out.println("Bilgi girmek ister misiniz?");
+        bilgi = scanner.nextInt();
+    
+          }
+          
+   
+     
+  
+     
+      }
+      
+      //--WRITTEN BY MERT MAHANOĞLU
+      
+      
+    public static void main(String[] args) {
+        
+      BinarySearchTree tree = new BinarySearchTree();
+      tree.bilgiInput(tree);
+      
+    
+     
+     
+        }
+           
+     
+     
+     
+     
+     
+     
+     
+    /* bstTree.root = new Node( ad, adres, telNo, mail, dogTar, dil, sure, ehliyet, isAd, adres, telNo, isAd, sure, adOkul, bolum, basTar, bitTar, notOrt);
      
         if (ad.compareTo(bstTree.BSTad)==0) {
               bstTree.root.sol = new Node( ad, adres, telNo, mail, dogTar, dil, sure, ehliyet, isAd, adres, telNo, isAd, sure, adOkul, bolum, basTar, bitTar, notOrt);
@@ -210,18 +354,20 @@ gecicidugum.sag = sag; // Bagi listeden kes
         }
         else
         {
-         bstTree.root.sag = new Node( ad, adres, telNo, mail, dogTar, dil, sure, ehliyet, isAd, adres, telNo, isAd, sure, adOkul, bolum, basTar, bitTar, notOrt);
+         bstTree.root.sag = new Node( ad, adres, telNo, mail, dogTar, dil, sure, ehliyet, isAd, adres, telNo, isAd, sure, adOkul, bolum, basTar, bitTar, notOrt);*/
         }   
              
     
- bstTree.BstToLinked(bstTree.root);
+ /*bstTree.BstToLinked(bstTree.root);
  bstTree.listeyiYazdir(bstTree.head);
-        System.out.println("Çalışıyor mu");
+ System.out.println("Çalışıyor mu");*/
+    
+    
+    
  
  
-    }
     
     
     
     
-}
+    
--- src/Node.java
+++ src/Node.java
@@ -18,6 +18,8 @@ public class Node {
     //----------------- KİŞİ BİLGİLERİ --------------
     Node sol;
     Node sag;
+    
+    
     String ad;
     String adres;
     String telefonNo;
@@ -45,8 +47,10 @@ public class Node {
     String bitisTarih;
     double notOrt;
 
-    public Node( String ad, String adres, String telefonNo, String email, String dogumTarihi, String yabanciDil, int yabanciDilSay, String ehliyet, String isYeriAd, String isYeriAdres, String isYeriTelefon, String pozisyon, int calismaSuresi, String okulAd, String bolum, String baslangicTarih, String bitisTarih, double notOrt) {
+    public Node(String ad, String adres, String telefonNo, String email, String dogumTarihi, String yabanciDil, int yabanciDilSay, String ehliyet, String isYeriAd, String isYeriAdres, String isYeriTelefon, String pozisyon, int calismaSuresi, String okulAd, String bolum, String baslangicTarih, String bitisTarih, double notOrt) {
        
+        sol=sag=null;
+        
         this.ad = ad;
         this.adres = adres;
         this.telefonNo = telefonNo;
@@ -67,6 +71,166 @@ public class Node {
         this.notOrt = notOrt;
         
         sol=sag=null;
+    }
+
+    public Node getSol() {
+        return sol;
+    }
+
+    public void setSol(Node sol) {
+        this.sol = sol;
+    }
+
+    public Node getSag() {
+        return sag;
+    }
+
+    public void setSag(Node sag) {
+        this.sag = sag;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public String getTelefonNo() {
+        return telefonNo;
+    }
+
+    public void setTelefonNo(String telefonNo) {
+        this.telefonNo = telefonNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDogumTarihi() {
+        return dogumTarihi;
+    }
+
+    public void setDogumTarihi(String dogumTarihi) {
+        this.dogumTarihi = dogumTarihi;
+    }
+
+    public String getYabanciDil() {
+        return yabanciDil;
+    }
+
+    public void setYabanciDil(String yabanciDil) {
+        this.yabanciDil = yabanciDil;
+    }
+
+    public int getYabanciDilSay() {
+        return yabanciDilSay;
+    }
+
+    public void setYabanciDilSay(int yabanciDilSay) {
+        this.yabanciDilSay = yabanciDilSay;
+    }
+
+    public String getEhliyet() {
+        return ehliyet;
+    }
+
+    public void setEhliyet(String ehliyet) {
+        this.ehliyet = ehliyet;
+    }
+
+    public String getIsYeriAd() {
+        return isYeriAd;
+    }
+
+    public void setIsYeriAd(String isYeriAd) {
+        this.isYeriAd = isYeriAd;
+    }
+
+    public String getIsYeriAdres() {
+        return isYeriAdres;
+    }
+
+    public void setIsYeriAdres(String isYeriAdres) {
+        this.isYeriAdres = isYeriAdres;
+    }
+
+    public String getIsYeriTelefon() {
+        return isYeriTelefon;
+    }
+
+    public void setIsYeriTelefon(String isYeriTelefon) {
+        this.isYeriTelefon = isYeriTelefon;
+    }
+
+    public String getPozisyon() {
+        return pozisyon;
+    }
+
+    public void setPozisyon(String pozisyon) {
+        this.pozisyon = pozisyon;
+    }
+
+    public int getCalismaSuresi() {
+        return calismaSuresi;
+    }
+
+    public void setCalismaSuresi(int calismaSuresi) {
+        this.calismaSuresi = calismaSuresi;
+    }
+
+    public String getOkulAd() {
+        return okulAd;
+    }
+
+    public void setOkulAd(String okulAd) {
+        this.okulAd = okulAd;
+    }
+
+    public String getBolum() {
+        return bolum;
+    }
+
+    public void setBolum(String bolum) {
+        this.bolum = bolum;
+    }
+
+    public String getBaslangicTarih() {
+        return baslangicTarih;
+    }
+
+    public void setBaslangicTarih(String baslangicTarih) {
+        this.baslangicTarih = baslangicTarih;
+    }
+
+    public String getBitisTarih() {
+        return bitisTarih;
+    }
+
+    public void setBitisTarih(String bitisTarih) {
+        this.bitisTarih = bitisTarih;
+    }
+
+    public double getNotOrt() {
+        return notOrt;
+    }
+
+    public void setNotOrt(double notOrt) {
+        this.notOrt = notOrt;
     }
      
     
